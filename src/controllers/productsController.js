9fee44b708336b45631883f833b963772c7b4d4b
javalvarenga <abraalvare@gmail.com>
2024-10-21 19:41:32 -0600

    remove keys

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,19 +1,37 @@
 const Product = require("../models/productModel");
+const { sequelize } = require("../db");
+const { decodeBase64Image, isBase64DataUrl } = require("../utils");
+const { uploadImage } = require("../utils/s3");
 
 exports.createProduct = async (req, res) => {
   try {
-    const { ProductName, Price, Quantity, Description } = req.body;
+    const { ProductName, Price, Quantity, Description, Image } = req.body;
+    
+    if (Image && Image.length > 0) {
+      const isBase64 = isBase64DataUrl(Image);
+      const ImageUrl = isBase64
+        ? await uploadImage(await decodeBase64Image(Image))
+            .then(async (s3Url) => s3Url)
+            .catch((error) => {
+              console.log("error", error);
+              throw new Error("Error al guardar la imagen");
+            })
+        : null;
+        console.log('ImageUrl', ImageUrl);
 
-    // Crear un nuevo producto
-    const newProduct = await Product.create({
-      ProductName,
-      Price,
-      Quantity,
-      Description,
-    });
+      // Crear un nuevo producto
+      const newProduct = await Product.create({
+        ProductName,
+        Price,
+        Quantity,
+        Description,
+        ImageUrl,
+      });
 
-    res.status(201).json(newProduct); // 201 indica que el recurso fue creado exitosamente
+      res.status(201).json(newProduct); // 201 indica que el recurso fue creado exitosamente
+    }
   } catch (error) {
+        console.log('error',error);
     res.status(500).json({ message: "Error al crear el producto", error });
   }
 };
@@ -88,5 +106,23 @@ exports.deleteProduct = async (req, res) => {
     res.json({ message: "Producto eliminado correctamente" });
   } catch (error) {
     res.status(500).json({ message: "Error al eliminar el producto", error });
+  }
+};
+
+exports.reportByProduct = async (req, res) => {
+  const { productId } = req.body; // O obtener los parámetros de req.query o req.params
+
+  try {
+    const results = await sequelize.query(
+      "CALL reportByProduct(:productId)", // Llamada al procedimiento almacenado
+      {
+        replacements: { productId }, // Pasar los parámetros
+        type: sequelize.QueryTypes.RAW, // Tipo de consulta RAW
+      }
+    );
+
+    res.json(results); // Enviar los resultados como respuesta
+  } catch (error) {
+    res.status(500).json({ message: "Error al ejecutar el reporte", error });
   }
 };
