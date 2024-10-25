75cb32f9f9c8380539a85bd208b145f452b17aa2
SacalF <frislymanuel07@gmail.com>
2024-10-25 11:22:39 -0600

    Devoluciones Backend

--- src/controllers/returnsController.js
+++ src/controllers/returnsController.js
@@ -1,35 +1,47 @@
-const { sequelize } = require("../db"); // Importa la conexión a la base de datos
+const sequelize = require("../db"); // Importa la conexión a la base de datos
 const Devolucion = require("../models/returnsModel");
 
 // Controlador para insertar una devolución
 exports.createDevolucion = async (req, res) => {
+  const { id_venta, ProductId, motivo } = req.body;
   try {
-    const { id_venta, ProductId, motivo } = req.body;
-
-    await sequelize.query(
+    const results = await sequelize.query(
       "CALL InsertarDevolucion(:id_venta, :ProductId, :motivo)", 
       {
-        replacements: {
-          id_venta,
-          ProductId,
-          motivo,
-        },
+        replacements: { id_venta, ProductId, motivo }, 
+        type: sequelize.QueryTypes.RAW, 
       }
     );
 
-    res.status(201).json({ message: "Devolución creada exitosamente" });
+    // Enviar respuesta de éxito
+    res.status(201).json({ message: "Devolución creada correctamente", data: results });
   } catch (error) {
-    res.status(500).json({ message: "Error al crear la devolución", error });
+    console.error("Error al crear la devolución:", error); 
+    res.status(500).json({ message: "Error al crear la devolución", error: error.message || error });
   }
 };
 
 // Controlador para listar devoluciones
 exports.getDevoluciones = async (req, res) => {
   try {
-    const devoluciones = await sequelize.query("CALL ListarDevoluciones()");
+    // Llama al procedimiento almacenado ListarDevoluciones
+    const results = await sequelize.query(
+      "CALL ListarDevoluciones()", 
+      {
+        type: sequelize.QueryTypes.RAW,
+      }
+    );
 
+    const devoluciones = results
+
+    if (devoluciones.length === 0) {
+      return res.status(404).json({ message: "No se encontraron devoluciones." });
+    }
+
+    // Enviar respuesta con las devoluciones
     res.json(devoluciones);
   } catch (error) {
-    res.status(500).json({ message: "Error al obtener las devoluciones", error });
+    console.error("Error al obtener las devoluciones:", error); // Muestra el error en la consola
+    res.status(500).json({ message: "Error al obtener las devoluciones", error: error.message || error });
   }
 };
