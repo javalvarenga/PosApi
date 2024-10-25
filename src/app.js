006b51cfba073c273931c944a30b5952f31d4ac2
javalvarenga <abraalvare@gmail.com>
2024-10-24 19:23:15 -0600

    login and categories

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const products = require('./routes/products');
 const sales = require('./routes/sales');
 const categories = require('./routes/categories');
+const users = require('./routes/users');
 
 const cors = require('cors');
 const PORT = process.env.PORT || 3001;
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(`/${version}/products`, products);
 app.use(`/${version}/sales`, sales);
 app.use(`/${version}/categories`, categories);
+app.use(`/${version}/users`, users);
 
 
 app.get('/', (req, res) => {
